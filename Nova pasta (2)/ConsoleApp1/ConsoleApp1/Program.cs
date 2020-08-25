a1443497b635dceedc31b32bdc6e22782ed78c57
stephany g sodre <69463672+stephanysodre@users.noreply.github.com>
2020-08-25 14:56:40 -0300

    Add files via upload

---
+++ Nova pasta (2)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Dados do funcionário:");
+            Console.Write("Nome: ");
+            f1.name = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do outro funcionário:");
+            Console.Write("Nome: ");
+            f2.name = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double media = (f1.sal + f2.sal) / 2.0;
+            Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+
+}
---
+++ Nova pasta (2)/atividade 1/atividade 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace atividade_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pessoa n1 = new pessoa();
+            pessoa n2 = new pessoa();
+
+            Console.WriteLine("insira seus dados: ");
+            Console.WriteLine("nome: ");
+            n1.name = Console.ReadLine();
+            Console.WriteLine("idade: ");
+            n1.age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira os dados da outra pessoa: ");
+            Console.WriteLine("nome: ");
+            n2.name = Console.ReadLine();
+            Console.WriteLine("idade: ");
+            n2.age = int.Parse(Console.ReadLine());
+
+            if (n1.age > n2.age)
+            {
+                Console.WriteLine("o mais velho é: " + n1.name);
+            }
+            else
+            {
+                Console.WriteLine("o mais velho é: " + n2.name);
+            }
+        }
+    }
+}
